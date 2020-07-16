20b796686836c03249899d49b5abd83ff714d784
andrefdre <58526188+andrefdre@users.noreply.github.com>
2020-07-16 16:41:34 +0100

    1

--- Project/app.js
+++ Project/app.js
@@ -5,6 +5,18 @@ var serv =require('http').Server(app);
 app.get('/',function(req,res) {
 res.sendFile(__dirname + '/client/index.html');
 });
+
 app.use('/client',express.static(__dirname + '/client'));
 
 serv.listen(2000);	
+
+var io =require('socket.io')(serv,{});
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('join',function(data){
+	  console.log(data.reason);
+  });
+});
+
+
+
